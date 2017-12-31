812fbe2d988d9274d43f9bb504deeb41ac81a0f4
Alexandre Dulaunoy <a@foo.be>
2017-12-31 21:28:22 +0100

    First release of a minimal standalone sighting server

---
+++ README.md
@@ -0,0 +1,95 @@
+# misp-sighting-server
+
+MISP sighting server is a fast sighting server to store and look-up sightings on attributes (network indicators, file hashes, system indicators) in a
+space efficient way.
+
+## Features
+
+- Simple ReST API to get or set sighting
+- TODO - fast DNS server to allow lookup of sighting via DNS queries
+- TODO - fast import of sighting via pub-sub channel like ZMQ
+
+## Back-end database
+
+MISP sighting server rely on a [ardb](https://github.com/yinqiwen/ardb) using [RocksDB](https://github.com/facebook/rocksdb) at the current stage. The back-end database might
+change following the evolution of the requirements or capabilities but the objective is to keep a compatibility layer to ensure use of the sighting database have the same
+API on the long-term.
+
+# Install
+
+~~~
+git submodule init
+git submodule update
+cd back-end/ardb
+storage_engine=rocksdb make
+cd ../..
+pip3 install -r REQUIREMENTS
+~~~
+
+# Starting the servers
+
+## Starting the back-end
+
+~~~
+cd back-end/ardb
+./src/ardb-server ardb.conf
+~~~
+
+## Starting the ReST API server
+
+~~~
+cd ./bin/
+python3 sighting-server.py
+~~~
+
+# Testing
+
+## Add a sighting
+
+~~~
+curl --header 'X-API-Key: afdef83f9cc7c87b801c36e4af632ef06af2f2ef' -X PUT  http://127.0.0.1:5000/add -d "value=127.0.0.1"  -d "source=honeypot"
+~~~
+
+## Get a sighting
+
+~~~
+curl -X GET http://127.0.0.1:5000/get -d "value=127.0.0.1"
+{
+    "1514737886": "honeypot",
+    "1514736878": "unknown",
+    "1514737898": "honeypot",
+    "1514737866": "honeypot",
+    "1514750757": "honeypot",
+    "1514736844": "unknown",
+    "1514737114": "blackhole",
+    "1514750755": "honeypot",
+    "1514750799": "honeypot",
+    "1514736877": "unknown",
+    "1514736840": "honeypot",
+    "1514737641": "honeypot",
+    "1514750760": "honeypot",
+    "1514737028": "unknown",
+    "1514736904": "unknown",
+    "1514737110": "blackhole",
+    "1514737219": "honeypot",
+    "1514737900": "honeypot",
+    "1514737901": "honeypot",
+    "1514737216": "honeypot",
+    "1514737102": "honeypot",
+    "1514736900": "honeypot",
+    "1514735898": "honeypot",
+    "1514736727": "honeypot",
+    "1514737113": "blackhole",
+    "1514737841": "honeypot",
+    "1514737111": "blackhole",
+    "1514750756": "honeypot",
+    "1514736848": "honeypot",
+    "1514737709": "honeypot",
+    "1514737115": "blackhole",
+    "1514737680": "honeypot",
+    "1514735899": "honeypot",
+    "1514737117": "blackhole",
+    "1514737109": "blackhole"
+}
+~~~
+
---
+++ bin/sighting-server.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#    ReST server for misp sighting server
+#
+#    Copyright (C) 2017 Alexandre Dulaunoy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+import redis
+import datetime
+import pytz
+import time
+import configparser
+
+app = Flask(__name__)
+api = Api(app)
+
+cfg = configparser.ConfigParser()
+cfg.read('../cfg/server.cfg')
+
+ardb_port = cfg.get('server', 'ardb_port')
+ardb_socket = cfg.get('server', 'ardb_socket')
+api_key = cfg.get('server', 'api_key')
+default_source = cfg.get('server', 'default_source')
+
+version = '0.1'
+r = redis.StrictRedis(unix_socket_path=ardb_socket, db=0, decode_responses=True,
+                      charset='utf-8')
+
+def Init():
+    r.set('misp-sighting-server:version', version)
+    d = datetime.datetime.utcnow()
+    d_with_timezone = d.replace(tzinfo=pytz.UTC)
+    r.set('misp-sighting-server:startup', d_with_timezone.isoformat())
+
+def TestBackend(version=version):
+    version = r.get('misp-sighting-server:version')
+    startup = r.get('misp-sighting-server:startup')
+    return (version, startup)
+
+
+class GetStatus(Resource):
+    def get(self):
+        status = TestBackend()
+        return {'version': status[0], 'startup': status[1]}
+
+class AddSighting(Resource):
+    def put(self):
+        if request.form['value'] is None:
+            return False
+        if not (request.headers.get('X-Api-Key') == api_key):
+            print (request.headers)
+            return False
+        if not request.form.get('epoch'):
+            when = int(time.time())
+        else:
+            when = request.form['epoch']
+        if not request.form.get('source'):
+            source = default_source
+        else:
+            source = request.form['source']
+        if r.hset(request.form['value'], when,
+                  source):
+            return True
+        else:
+            return False
+
+class GetSighting(Resource):
+    def get(self):
+        if request.form['value'] is None:
+            return False
+        return r.hgetall(request.form['value'])
+
+api.add_resource(GetStatus, '/')
+api.add_resource(AddSighting, '/add')
+api.add_resource(GetSighting, '/get')
+
+if __name__ == '__main__':
+    Init()
+    app.run(debug=True)
